a12908c9f34aaa984468b6fff3cdbac255d41904
Emilis Kleinas <emiliskleinas2003@gmail.com>
2025-04-06 23:28:18 +0300

    SCRUM-1 remove request path error

--- PSK2025.Frontend/src/api/requests/randomNumberRequest.ts
+++ PSK2025.Frontend/src/api/requests/randomNumberRequest.ts
@@ -3,6 +3,6 @@ import { RandomNumber } from "../types/RandomNumber";
 
 // TODO: Add error handling
 export const fetchRandomNumber = async (): Promise<RandomNumber> => {
-    const response = await api.get<RandomNumber>("/api/RandomNumber");
+    const response = await api.get<RandomNumber>("/RandomNumber");
     return response.data;
 };
